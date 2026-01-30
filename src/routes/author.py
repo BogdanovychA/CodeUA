8597af684c8bc6d5e9a145aace3cc7622af4f62d
Andrii Bogdanovych <a@bogdanovych.org>
2026-01-30 14:37:46 +0200

    Замінено фото автора

--- src/routes/author.py
+++ src/routes/author.py
@@ -32,7 +32,7 @@ def build_view(page: ft.Page) -> ft.View:
             elements.app_bar(TITLE, page),
             ft.Text(""),
             ft.Image(
-                src="/images/bogdanovych-950x950.jpg",
+                src="/images/bogdanovych-900x900.png",
                 width=200,
                 height=200,
             ),
