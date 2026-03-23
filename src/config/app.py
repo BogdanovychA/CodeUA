0f0c88d866c3ccb5c092554e91437d35107e25b1
Andrii Bogdanovych <a@bogdanovych.org>
2026-03-23 16:44:44 +0200

    Виправлено залежності

--- src/config/app.py
+++ src/config/app.py
@@ -14,7 +14,7 @@ class Settings(BaseModel):
         return Path(os.environ.get("FLET_ASSETS_DIR", default_assets_dir)).resolve()
 
     name: str = "CodeUA"
-    version: str = "1.2.2"
+    version: str = "1.2.3"
 
     base_url: str = ""
     assets_dir: Path = get_asset_dir()
